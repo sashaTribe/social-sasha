7ed5c02d9ecda1116dc3d87e1aa7ffa1d1800c8f
sashaTribe <sasha555.tribe@gmail.com>
2022-11-06 16:50:49 +0000

    added seeder for userdata

--- database/migrations/2022_11_06_161446_create_user_data_table.php
+++ database/migrations/2022_11_06_161446_create_user_data_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('username');
             $table->string('password');
-            $table->string("email");
+            $table->string('email');
             $table->timestamps();
         });
     }
