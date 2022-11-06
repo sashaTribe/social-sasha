4ada29692ccb52a6856fc9a47497473b16d90f56
sashaTribe <sasha555.tribe@gmail.com>
2022-11-06 23:09:17 +0000

    ammended comment attribute (limit char to 255)

--- database/migrations/2022_11_06_154638_create_comments_table.php
+++ database/migrations/2022_11_06_154638_create_comments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('comment');
+            $table->string('comment',255);
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('parent_id')->unsigned;
             $table->timestamps();
