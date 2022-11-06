2351933a26eadde78bcc5cca2b2f8c9f314ca3f9
sashaTribe <sasha555.tribe@gmail.com>
2022-11-06 17:21:42 +0000

    created 50 fake accounts

--- database/seeders/UserDataTableSeeder.php
+++ database/seeders/UserDataTableSeeder.php
@@ -15,12 +15,7 @@ class UserDataTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-        $user1 = new UserData;
-        $user1->username = "sahsa";
-        $user1->password = "122444";
-        $user1->email = "sasha@gmail.com";
-        $user1->save();
-
+        //e
+        UserData::factory()->count(50)->create();
     }
 }
