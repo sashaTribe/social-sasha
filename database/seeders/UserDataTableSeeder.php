720821f241ea2c937ae78e87605f5dccdc3b22b6
sashaTribe <sasha555.tribe@gmail.com>
2022-11-06 16:56:42 +0000

    created seeder for UserData

--- database/seeders/UserDataTableSeeder.php
+++ database/seeders/UserDataTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\UserData;
 
 class UserDataTableSeeder extends Seeder
 {
@@ -15,5 +16,11 @@ class UserDataTableSeeder extends Seeder
     public function run()
     {
         //
+        $user1 = new UserData;
+        $user1->username = "sahsa";
+        $user1->password = "122444";
+        $user1->email = "sasha@gmail.com";
+        $user1->save();
+
     }
 }
