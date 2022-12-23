25220ca3175bb87a8a7054f8dd998df58b0c4805
sashaTribe <sasha555.tribe@gmail.com>
2022-12-23 01:09:31 +0000

    php myadmin

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,14 @@
-use App\Models\UserData;
-@extends('layouts.app');
+
+@extends('layouts.app')
 
 @section('title', 'Users')
 
 @section('content')
     <p>Users in Social Sasha: </p>
     <ul>
-        @foreach($users as $user)
-            <li>{{ $user -> username}}</li>
-        @endforeach
-    </ul>
+    @foreach($users as $user)
+        <li>{{ $user -> firstName}}</li>
+    @endforeach
+    </ul> 
+    
+    
